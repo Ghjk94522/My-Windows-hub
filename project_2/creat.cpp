2550c6a1b325fe18b4b28d56b57de9ec15b1e5ca
Ghjk945 <2500223255@qq.com>
2019-04-29 18:14:20 +0800

    add a little edit for creat

--- project_2/creat.cpp
+++ project_2/creat.cpp
@@ -4,7 +4,16 @@
 #include<vector>
 using namespace std;
 
+extern string account;
+
 vector<string> column;
+
+/*
+这里加一些注释，用全局变量account引入对每个用户数据库的表名和对应文件地址的记录
+其中t_name表示表名，acc_address表示地址
+顺序也是表名 \t 地址
+每次使用creat新的表的时候都要先记录表名和地址的一一对应
+*/
 
 void creat_point(){
     string rubbish; cin >> rubbish;
@@ -29,13 +38,15 @@ void creat_point(){
     }
     
     cin >> rubbish;
-    string address; cin >> address; //address -= ".txt";
-    address = "/home/young/progExper/DataHub/" + address;
+    string address; cin >> address; 
+    address = substr(0, address.length() - 4);
+    string address_root; address_root = "/home/young/progExper/RootHub/" + address + "Root.txt";
+    address = "/home/young/progExper/DataHub/" + address + ".txt";
 
-    const char *p = address.data();
+    const char *p0 = address.data();
 
     FILE *fp;
-    fp = fopen(p, "a");
+    fp = fopen(p0, "a");
 
     fprintf(fp, t_name.c_str());
     fprintf(fp, "\n\n");
@@ -46,6 +57,26 @@ void creat_point(){
     }
 
     fclose(fp);
+    
+    string acc_address;
+    acc_address = account + "SQL.txt";
+    const char *p1 = acc_address.data();
+    FILE *fp = fopen(p1, "a");
+
+    fprintf(fp, "\n");
+    fprintf(fp, t_name->c_str());
+    fprintf(fp, "\t");
+    fprintf(fp, address->c_str());
+
+    fclose(fp);
+
+    const char *q = address_root.data();
+    FILE *fp = fopen(q, "a");
+    fprintf(fp, "owner\t1\t1\t1\t1\t");//这里四个权限数字分别对应DROP/INSERT/DELETE/SELETE，用1表示拥有，0表示不拥有
+    fclose(fp);
+
+    
+
     }
 
     /*else if(temp == 'F'){
