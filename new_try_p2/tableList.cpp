653611f7247474cd7256b261038166fd12148e69
Ghjk945 <2500223255@qq.com>
2019-05-21 23:12:54 +0800

    add a file of function of dividing the string

--- new_try_p2/tableList.cpp
+++ new_try_p2/tableList.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<fstream>
 using namespace std;
 
 extern string account;
@@ -9,10 +10,16 @@ void table_list(){
 	string rubbish;
 	cin >> rubbish;
 
+	if(rubbish != "LIST"){
+		cout << "WRONG ORDER!!!\n";
+		print_sql();
+		check_order();
+	}
+	vector<string> tableName(10);
+
+
 	string acc_address = "/home/ubuntu/yyy/project_2/RootHub/" + account + "SQL.txt";
 	const char *p = acc_address.data();
 
-	freopen(p, "r", stdin);
-	cin >>
-
+	
 }
